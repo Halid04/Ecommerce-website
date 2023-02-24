7124b737b6626415c61b94d6789470cdadf66d7a
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2023-02-24 20:16:43 +0100

    Add files via upload

---
+++ AboutUSPage/aboutUs.js
@@ -0,0 +1,4 @@
+const aboutUsLink= document.querySelector(".AboutUs");
+aboutUsLink.classList.add("border-bottom-animation");
+aboutUsLink.classList.add("border-bottom-animation::after");
+// aboutUsLink.classList.add("border-bottom-animation:hover::after");
