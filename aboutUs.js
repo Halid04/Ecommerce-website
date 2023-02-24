312ba683573b3af2b084b28a2e5ab911c8a340ab
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2023-02-24 20:15:16 +0100

    Delete aboutUs.js

--- aboutUs.js
+++
@@ -1,4 +0,0 @@
-const aboutUsLink= document.querySelector(".AboutUs");
-aboutUsLink.classList.add("border-bottom-animation");
-aboutUsLink.classList.add("border-bottom-animation::after");
-// aboutUsLink.classList.add("border-bottom-animation:hover::after");
