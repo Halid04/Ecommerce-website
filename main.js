3c6ec5a50fb6598274cdf31323f7928439c290f1
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2023-03-06 13:50:07 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const openMenuBar = document.querySelector(".menu-bar img")
+const mobileMenu = document.querySelector(".mobileMenu");
+const closeMenuBar = document.querySelector(".close img")
+
+openMenuBar.addEventListener("click", ()=>{
+    console.log("ok");
+    mobileMenu.style.transform = "translateX(0%)"
+})
+
+closeMenuBar.addEventListener("click", ()=>{
+    mobileMenu.style.transform = "translateX(100%)"
+})
